c2dc9bd7ca77d5e0308b664dc4043559c25e8350
Ilya Lebedev <ilyalebedev1@yandex.ru>
2018-11-10 06:40:55 +0300

    results completed

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -5,11 +5,19 @@ class Entrant
 
   store_in collection: 'results'
 
-  embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc]
+  embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
 
   field :bib, type: Integer
   field :secs, type: Float
   field :o, as: :overall, type: Placing
   field :gender, type: Placing
   field :group, type: Placing
+
+  def update_total(result)
+  	sum = 0
+  	results.each do |res|
+  	  sum += res.secs
+  	end
+    self.secs=sum
+  end
 end
